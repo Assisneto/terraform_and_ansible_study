ff1324a1c49d29315a7efa7769329445544f940d
Assisneto Damasceno <netodamasceno12@gmail.com>
2023-01-11 16:08:46 -0300

    iniciando um servidor http atraves do tf

--- main.tf
+++ main.tf
@@ -17,7 +17,13 @@ resource "aws_instance" "app_server" {
   ami           = "ami-03f8756d29f0b5f21"
   instance_type = "t2.micro"
   key_name      = "terraform-deploy-oregon"
+  user_data     = <<-EOF
+                    #!/bin/bash
+                    cd /home/ubuntu
+                    echo "<h1>Feito com Terraform</h1>" > index.html
+                    nohup busybox httpd -f -p 8080 &
+                    EOF
   tags = {
-    Name = "first instance"
+    Name = "second instance"
   }
 }
